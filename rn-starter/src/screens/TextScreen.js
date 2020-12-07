ca2ad3dd5522daa2486c3352f79b2c4008c80e09
edgarciaalamilla <eddiegarcia.cs@gmail.com>
2020-12-07 17:47:16 -0600

    Add TextScreen

---
+++ rn-starter/src/screens/TextScreen.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+
+const TextScreen = () => {
+  const [name, setName] = useState("");
+
+  return (
+    <View>
+      <Text>Enter Password:</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={name}
+        onChangeText={(newValue) => setName(newValue)}
+      />
+      {name.length > 4 ? null : (
+        <Text>Password must be at least 5 characters long.</Text>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: "black",
+    borderWidth: 1,
+  },
+});
+
+export default TextScreen;
